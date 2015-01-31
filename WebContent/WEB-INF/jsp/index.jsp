2de8aa1ebab555f1b9ce00456d1d65b7be190a2b
Phillip Friedrich <friedrich.phillip@gmail.com>
2015-01-31 20:36:55 +0100

    initial

---
+++ WebContent/WEB-INF/jsp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+  	<head>
+  		<title>Spring Webflow Tutorial</title>
+  	</head>
+
+  	<body>
+		<h2>Hello!</h2>
+	</body>
+</html>
--- src/pf/IndexController.java
+++ src/pf/IndexController.java
@@ -1,17 +1,15 @@
 package pf;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "test")
-public class TestController {
+@RequestMapping(value = "/")
+public class IndexController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<Void> test() {
-		return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+	public String index() {		
+		return "index";
 	}
 }
